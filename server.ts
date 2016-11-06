467be4d19042277387ec5c4b4908fec563760c37
intern03 <isd3@midsserver.com>
2016-11-06 13:27:41 +0430

    v1.0.0

--- server.ts
+++
@@ -1,7 +0,0 @@
-import * as express from 'express';
-import Sample from "./src/sample";
-// var app = express();
-Sample.init();
-// app.listen(3000,()=>{
-//     console.log('Listening of port 3000');
-// });
